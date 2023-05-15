79a4455125191e4045a33573b96345f1dd28e92a
jinWan0823 <dhksl823@gmail.com>
2023-05-15 21:11:30 +0900

    Lifecycle 클래스형 컴포넌트 실습

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,40 @@
-import './App.css';
-// import Counter from './components/Counter'
-import Calculator from './components/Calculator';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <Calculator />
-    </div>
-  );
+export default class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+  }
+
+  tick() {
+    this.setState({
+      date : new Date(),
+    });
+  }
+
+  componentDidMount() {
+    console.log("componentDidMount");
+    this.timerID = setInterval(()=>this.tick(),1000);
+  }
+
+  componentDidUpdate() {
+    console.log("componentDidUpdate");
+    console.log(this.state.date);
+  }
+
+  componentWillUnmount() {
+    console.log("componentWillUnmount");
+    clearInterval(this.timerID);
+  }
+
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+
 }
-
-export default App;

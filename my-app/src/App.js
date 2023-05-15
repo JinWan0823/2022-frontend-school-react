13da9ea8e5686bc36288149386211df44db26499
jinWan0823 <dhksl823@gmail.com>
2023-05-15 21:43:39 +0900

    useEffect() hook 실습

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,40 +1,6 @@
 import React from "react";
+import ClassClock from "./components/FunctionClock";
 
-export default class Clock extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { date: new Date() };
-  }
-
-  tick() {
-    this.setState({
-      date : new Date(),
-    });
-  }
-
-  componentDidMount() {
-    console.log("componentDidMount");
-    this.timerID = setInterval(()=>this.tick(),1000);
-  }
-
-  componentDidUpdate() {
-    console.log("componentDidUpdate");
-    console.log(this.state.date);
-  }
-
-  componentWillUnmount() {
-    console.log("componentWillUnmount");
-    clearInterval(this.timerID);
-  }
-
-
-  render() {
-    return (
-      <div>
-        <h1>Hello, world!</h1>
-        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-      </div>
-    );
-  }
-
+export default function App() {
+  return <ClassClock />;
 }

fe55ffaccc9c5275b2e48d5902adf98f35a8ad90
jinWan0823 <dhksl823@gmail.com>
2023-05-17 18:20:39 +0900

    useRef를 이용한 상태관리. 리렌더링은 일어나지 않는다

--- my-app/src/components/CatParent.js
+++ my-app/src/components/CatParent.js
@@ -1,4 +1,4 @@
-import React , { useRef } from "react";
+import React , { useRef , useState} from "react";
 import Cat from "./Cat";
 
 // Cat 안에 있는 image 사이즈를 알아내기
@@ -8,9 +8,14 @@ export default function CatParent() {
   console.log("부모 컴포넌트 CatParent");
   console.log(catRef)
 
+  const ageRef = useRef(1)
+  const [state, setState] = useState(1);
+
+  console.log(ageRef)
+  console.log(ageRef.current)
+
   const catSize = () =>{
     alert(catRef.current.height);
-    console.log(catRef)
   }
 
   return (
@@ -18,7 +23,11 @@ export default function CatParent() {
       <h4> 고양이가 세상을 구한다 ️</h4>
       <div>
         <Cat ref={catRef}/>
+        <h3>나이 : {ageRef.current}</h3>
+        <h4>상태값 : {state}</h4>
         <button onClick={catSize}>이미지 사이즈</button>
+        <button onClick={() => ageRef.current = ageRef.current + 1}>새해</button>
+        <button onClick={() => setState(state+1)}>테스트</button>
       </div>
     </div>
   );

e6124a277eb049c7020a90ac8e56d171edf5e504
jinWan0823 <dhksl823@gmail.com>
2023-05-17 12:35:33 +0900

    클래스 컴포넌트 이벤트 처리 // this 바인딩

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
-import Banner from "./components/Banner";
+import Toggle from "./components/Toggle";
 
 export default function App() {
   return (
-    <Banner />
+    <Toggle />
   );
 }

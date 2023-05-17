b37d761fb5246ec7e0eb53ea30e990d25a5cd5aa
jinWan0823 <dhksl823@gmail.com>
2023-05-17 18:44:01 +0900

    콜백 ref 실습

--- my-app/src/components/CatParent.js
+++ my-app/src/components/CatParent.js
@@ -3,31 +3,31 @@ import Cat from "./Cat";
 
 // Cat 안에 있는 image 사이즈를 알아내기
 export default function CatParent() {
-  const catRef = useRef();
-  
-  console.log("부모 컴포넌트 CatParent");
-  console.log(catRef)
+  // const catRef = useRef();
 
-  const ageRef = useRef(1)
-  const [state, setState] = useState(1);
+  const [height, setHeight] = useState(0);
 
-  console.log(ageRef)
-  console.log(ageRef.current)
-
-  const catSize = () =>{
-    alert(catRef.current.height);
+  const catCallbackRef = (node) => {
+    if(node !== null) {
+      setHeight(node.getBoundingClientRect().height)
+    }
   }
+
+  console.log("부모 컴포넌트 CatParent");
+  // console.log(catRef);
+
+  const ageRef = useRef(1);  
 
   return (
     <div>
       <h4> 고양이가 세상을 구한다 ️</h4>
       <div>
-        <Cat ref={catRef}/>
+        <Cat ref={catCallbackRef}/>
         <h3>나이 : {ageRef.current}</h3>
-        <h4>상태값 : {state}</h4>
-        <button onClick={catSize}>이미지 사이즈</button>
+        <h4>키 : {height}</h4>
+        {/* <button onClick={() =>{alert(catRef.current.height)}}>이미지 사이즈</button> */}
         <button onClick={() => ageRef.current = ageRef.current + 1}>새해</button>
-        <button onClick={() => setState(state+1)}>테스트</button>
+        {/* <button onClick={() => setState(state+1)}>테스트</button> */}
       </div>
     </div>
   );

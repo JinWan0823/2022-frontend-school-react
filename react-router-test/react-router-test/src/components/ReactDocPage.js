ccea7282b3be98d5f9310dc253c6ddbf1223de65
jinWan0823 <dhksl823@gmail.com>
2023-05-31 16:32:34 +0900

    async await , fetch api를 이용한 데이터 불러오기

--- react-router-test/react-router-test/src/components/ReactDocPage.js
+++ react-router-test/react-router-test/src/components/ReactDocPage.js
@@ -1,16 +1,30 @@
-import React from "react";
+import React , {useEffect , useState} from "react";
 import { useParams, useNavigate} from "react-router-dom";
 
 export default function ReactDocPage() {
   // docId : 1,2,3....
   const params = useParams();
-  console.log(params);
   const navigate = useNavigate();
+
+  const [docs, setDocs] = useState([]);
+
+  useEffect(() => {
+    async function docData() {
+      const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.docId}`);
+      const result = await res.json();
+      setDocs(result);
+    }
+
+    docData();
+
+  },[])
+
   return (
     <>
       <h1 onClick={() => navigate("/")}>logo</h1>
       <div>
-        <h1>ReactDocPage ##{params.docId}</h1>
+        <h1>{docs ? docs.title : "Loading..."}</h1>
+        <p>{docs.body}</p>
       </div>
 
     </>
